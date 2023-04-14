082b216727add0e7e09dc1619ce8ac86e28e4c7c
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2023-04-14 13:56:19 +0900

    ca55: Cleanup build script
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- application_cpu/build_ca55.sh
+++ application_cpu/build_ca55.sh
@@ -4,41 +4,26 @@ export PATH=~/.local/bin:$PATH
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 
 AOS_VERSION="v1.0.0"
-DOMD_OLD_SIZE="1024 MiB"
-DOMD_NEW_SIZE="2048 MiB"
-IMAGE_INSTALL_DOMD="        - [IMAGE_INSTALL_append, \" waii packagegroup-system-monitor iproute2-tc snort docker python3-docker-compose dhrystone whetstone sysbench iccom-lib iccom-test \"]"
-IMAGE_INSTALL_DOMU="        - [IMAGE_INSTALL_append, \" iproute2 iproute2-tc\"]"
-DOMD_NEW_FILE="        - \"tmp/work/%{MACHINE}-poky-linux/rcar-image-minimal/1.0-r0/rootfs/var/statestorage.db\""
-DOM0_BITBAKE_NEW_LAYER="        - \"../../../meta-rcar-whitebox/meta-aos-iccom\""
-DOMD_BITBAKE_NEW_LAYER="        - \"../../../meta-rcar-whitebox/meta-rcar-common\"\n        - \"../../../meta-rcar-whitebox/meta-xt-iccomlayer\""
-DOMD_VAR_NEW_FILE="          \"statestorage.db\": \"%{YOCTOS_WORK_DIR}/build-domd/tmp/work/%{MACHINE}-poky-linux/rcar-image-minimal/1.0-r0/rootfs/var/statestorage.db\""
-OLD_COMMIT=d1e67027b82143bd56cc8a881e11e4d475dcffb9
-NEW_COMMIT=e74fea0be1d2553617f13344babdfbeb0cee4eeb
 
-# Download yaml file
+# Prepare working directory
 cd ${SCRIPT_DIR}
+rm -rf ./work
+mkdir -p ./work
+cd ./work
+
+# Preprae yaml file
 rm -f aos-rcar-gen4.yaml
 curl -O https://raw.githubusercontent.com/aoscloud/meta-aos-rcar-gen4/${AOS_VERSION}/aos-rcar-gen4.yaml
+cat ./aos-rcar-gen4.yaml ../aos-rcar-gen4-patch.yaml > ./aos-rcar-gen4-wb.yaml
 
+#############################################
+# START: Apply patch for meta-aos-rcar-gen4 #
+#############################################
 # Remove meta-aos-rcar-gen4 from repo list
 PARTERN='    - type: git
       url: "https://github.com/aoscloud/meta-aos-rcar-gen4.git"
       rev: "v1.0.0"'
-sed -i -z "s|${PARTERN//$'\n'/\\n}||" aos-rcar-gen4.yaml
-
-# Update yaml file
-sed -i "s/${DOMD_OLD_SIZE}/${DOMD_NEW_SIZE}/" aos-rcar-gen4.yaml
-sed -i "s|\(\".unprovisioned\":.*\"\)|\1\n${DOMD_VAR_NEW_FILE}|" aos-rcar-gen4.yaml
-sed -i "s|\(  domu:$\)|${DOMD_NEW_FILE}\n\1|" aos-rcar-gen4.yaml
-sed -i "0,/\"..\/meta-aos\"/s|\(\"../meta-aos\"\)|\1\n${DOM0_BITBAKE_NEW_LAYER}|" aos-rcar-gen4.yaml
-sed -i "s|\(\"../meta-aos-rcar-gen4/meta-aos-rcar-gen4-driver-domain\"\)|\1\n${DOMD_BITBAKE_NEW_LAYER}|" aos-rcar-gen4.yaml
-sed -i "s|\(        - \[XT_DOM_NAME, \"domd\"\]$\)|\1\n${IMAGE_INSTALL_DOMD}|" aos-rcar-gen4.yaml
-sed -i "s|\(        - \[XT_DOM_NAME, \"domu\"\]$\)|\1\n${IMAGE_INSTALL_DOMU}|" aos-rcar-gen4.yaml
-
-# Prepare working directory
-rm -rf ./work
-mkdir -p ./work
-cd ./work
+sed -i -z "s|${PARTERN//$'\n'/\\n}||" ./aos-rcar-gen4-wb.yaml
 
 # Prepare additional repo
 mkdir -p ./yocto
@@ -48,11 +33,15 @@ cd ./yocto/meta-aos-rcar-gen4
 git checkout ${AOS_VERSION}
 cd ../../
 
+# Apply patch
 cd ./yocto/meta-aos-rcar-gen4
 git am ../../../patchset_aos/*
 cd ../../
+#############################################
+# END: Apply patch for meta-aos-rcar-gen4   #
+#############################################
 
-moulin ../aos-rcar-gen4.yaml
+moulin ./aos-rcar-gen4-wb.yaml
 ninja
 ninja image-full
 echo "Build finished !"
