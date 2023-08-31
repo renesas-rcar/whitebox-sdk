bfbfa511a35ea8c506579cc18fd50e19bb3df045
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2023-08-31 13:15:12 +0900

    Refactor build script
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- build_all.sh
+++ build_all.sh
@@ -2,9 +2,52 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 DEPLOY_DIR=${SCRIPT_DIR}/deploy
+TARGET_BOARD=""
+BOARD_LIST=("spider")
+
+#================================================
+# Usage
+#================================================
+Usage() {
+    echo "Usage:"
+    echo "    $0 board [option]"
+    echo "board:"
+    echo "    spider"
+    echo "option:"
+    echo "    -h: Show this usage"
+}
+
+#================================================
+# Check commandline args
+#================================================
+proc_args () {
+    for board in ${BOARD_LIST[*]}; do
+        if [[ "$board" == "$1" ]]; then
+            TARGET_BOARD=$1
+            shift 1
+        fi
+    done
+    if [[ -z ${TARGET_BOARD} ]]; then
+        Usage; exit;
+    fi
+
+    while getopts "h" OPT
+    do
+        case $OPT in
+            h) Usage; exit;;
+            *) echo Unsupported option; Usage; exit;;
+        esac
+            echo $OPT
+    done
+}
+
+#================================================
+# Main func
+#================================================
+proc_args $@
 
 CLEAN_BUILD_FLAG=true ${SCRIPT_DIR}/build_h3.sh
-CLEAN_BUILD_FLAG=true ${SCRIPT_DIR}/build_s4.sh
+CLEAN_BUILD_FLAG=true ${SCRIPT_DIR}/build_s4.sh $TARGET_BOARD
 
 # deploy
 rm -rf ${DEPLOY_DIR} && mkdir -p ${DEPLOY_DIR}
--- build_s4.sh
+++ build_s4.sh
@@ -4,81 +4,129 @@ export PATH=~/.local/bin:$PATH
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 AOS_VERSION="v1.0.0"
 WHITEBOX_VERSION="v3.1"
+TARGET_BOARD=""
+BOARD_LIST=("spider")
 
-# Check propieray package
 GUIDE_PATH=$(find ${SCRIPT_DIR}/proprietary -name "R-Car_S4_SDK_start_up_guide_content*.zip" | head -1)
 TOOL_PATH=$(find ${SCRIPT_DIR}/proprietary -name "rcar_tool_ubuntu*.zip" | head -1)
-err_msg () {
-    echo "ERROR: Proprietary packages are not found:"
-    echo "  - ${SCRIPT_DIR}/proprietary/R-Car_S4_SDK_start_up_guide_content*.zip"
-    echo "  - ${SCRIPT_DIR}/proprietary/rcar_tool_ubuntu*.zip"
-    echo ""
-    echo "Please download following files from renesas.com:"
-    echo "  - https://www.renesas.com/r-car-s4#downloads"
-    echo "    - R-Car S4 SDK Start Up Guide PKG v(VERSION)"
-    echo "    - R-Car S4 SDK common tool for ubuntu v(VERSION)"
-    echo "Note: version 3.14.0 or later is recommended"
+
+#================================================
+# Usage
+#================================================
+Usage() {
+    echo "Usage:"
+    echo "    $0 board [option]"
+    echo "board:"
+    echo "    spider"
+    echo "option:"
+    echo "    -h: Show this usage"
 }
-if [[ ! -e "${GUIDE_PATH}" ]]; then
-    err_msg; exit -1
-fi
-if [[ ! -e "${TOOL_PATH}" ]]; then
-    err_msg; exit -1
-fi
 
-# Move to working direcotry
-if [[ "${CLEAN_BUILD_FLAG:-false}" == "true" ]]; then
-    rm -rf ${SCRIPT_DIR}/work/s4_build
-fi
-mkdir -p ${SCRIPT_DIR}/work/s4_build
-cd ${SCRIPT_DIR}/work/s4_build
-
-# Setup yaml file
-curl -O https://raw.githubusercontent.com/aoscloud/meta-aos-rcar-gen4/${AOS_VERSION}/aos-rcar-gen4.yaml
-curl -O https://raw.githubusercontent.com/renesas-rcar/whitebox-sdk/${WHITEBOX_VERSION}/application_cpu/aos-rcar-gen4-patch.yaml
-cat aos-rcar-gen4.yaml aos-rcar-gen4-patch.yaml ${SCRIPT_DIR}/aos-rcar-gen4-demo-patch.yaml \
-    > ${SCRIPT_DIR}/work/s4_build/aos-rcar-gen4-demo.yaml
-
-# Remove meta-aos-rcar-gen4 from repo list
-PARTERN='    - type: git
-      url: "https://github.com/aoscloud/meta-aos-rcar-gen4.git"
-      rev: "v1.0.0"'
-apply_patch_meta-aos-rcar-gen4 () {
-    cd ${SCRIPT_DIR}/work/s4_build
-    sed -i -z "s|${PARTERN//$'\n'/\\n}||" ./aos-rcar-gen4-demo.yaml
-
-    # Prepare additional repo
-    mkdir -p ./yocto
-    if [[ ! -e "./yocto/meta-aos-rcar-gen4" ]]; then
-        git clone https://github.com/aoscloud/meta-aos-rcar-gen4 \
-            ./yocto/meta-aos-rcar-gen4
+#================================================
+# Check commandline args
+#================================================
+proc_args () {
+    for board in ${BOARD_LIST[*]}; do
+        if [[ "$board" == "$1" ]]; then
+            TARGET_BOARD=$1
+            shift 1
+        fi
+    done
+    if [[ -z ${TARGET_BOARD} ]]; then
+        Usage; exit;
     fi
-    cd ./yocto/meta-aos-rcar-gen4
-    git reset --hard ${AOS_VERSION}; git clean -df
-    PATCHSET=(
-        "0001-domd-Update-Linux-Kernel-to-latest-v5.10.41-rcar-5.1.patch"
-        "0002-domd-Add-Snort-and-required-S4-R-Switch-specific-lib.patch"
-        "0003-domd-Add-traffic-control-tool.patch"
-        "0004-README.md-Update-release-information-and-links.patch"
-        "0005-net-renesas-rswitch-fix-disabling-offload-with-defau.patch"
-    )
-    for patch in ${PATCHSET[@]}; do
-        URL=https://raw.githubusercontent.com/renesas-rcar/whitebox-sdk/${WHITEBOX_VERSION}/application_cpu/patchset_aos/${patch}
-        curl -s $URL | git apply
+
+    while getopts "h" OPT
+    do
+        case $OPT in
+            h) Usage; exit;;
+            *) echo Unsupported option; Usage; exit;;
+        esac
     done
 }
-apply_patch_meta-aos-rcar-gen4
 
-# Build image
-cd ${SCRIPT_DIR}/work/s4_build
-moulin ./aos-rcar-gen4-demo.yaml --BUILD_DOMD_SDK=no
-ninja
-ninja image-full
-gzip -kf full.img
-mv {,demo.}full.img.gz
+#================================================
+# Check propieray package
+#================================================
+check_proprietary_package_spider() {
+    err_msg () {
+        echo "ERROR: Proprietary packages are not found:"
+        echo "  - ${SCRIPT_DIR}/proprietary/R-Car_S4_SDK_start_up_guide_content*.zip"
+        echo "  - ${SCRIPT_DIR}/proprietary/rcar_tool_ubuntu*.zip"
+        echo ""
+        echo "Please download following files from renesas.com:"
+        echo "  - https://www.renesas.com/r-car-s4#downloads"
+        echo "    - R-Car S4 SDK Start Up Guide PKG v(VERSION)"
+        echo "    - R-Car S4 SDK common tool for ubuntu v(VERSION)"
+        echo "Note: version 3.14.0 or later is recommended"
+    }
+    if [[ ! -e "${GUIDE_PATH}" ]]; then
+        err_msg; exit -1
+    fi
+    if [[ ! -e "${TOOL_PATH}" ]]; then
+        err_msg; exit -1
+    fi
+}
+#================================================
+# build_sw
+#================================================
+build_sw() {
+    # Move to working direcotry
+    if [[ "${CLEAN_BUILD_FLAG:-false}" == "true" ]]; then
+        rm -rf ${SCRIPT_DIR}/work/s4_build
+    fi
+    mkdir -p ${SCRIPT_DIR}/work/s4_build
+    cd ${SCRIPT_DIR}/work/s4_build
 
+    # Setup yaml file
+    curl -O https://raw.githubusercontent.com/aoscloud/meta-aos-rcar-gen4/${AOS_VERSION}/aos-rcar-gen4.yaml
+    curl -O https://raw.githubusercontent.com/renesas-rcar/whitebox-sdk/${WHITEBOX_VERSION}/application_cpu/aos-rcar-gen4-patch.yaml
+    cat aos-rcar-gen4.yaml aos-rcar-gen4-patch.yaml ${SCRIPT_DIR}/aos-rcar-gen4-demo-patch.yaml \
+        > ${SCRIPT_DIR}/work/s4_build/aos-rcar-gen4-demo.yaml
+
+    # Remove meta-aos-rcar-gen4 from repo list
+    PARTERN='    - type: git
+          url: "https://github.com/aoscloud/meta-aos-rcar-gen4.git"
+          rev: "v1.0.0"'
+    apply_patch_meta-aos-rcar-gen4 () {
+        cd ${SCRIPT_DIR}/work/s4_build
+        sed -i -z "s|${PARTERN//$'\n'/\\n}||" ./aos-rcar-gen4-demo.yaml
+
+        # Prepare additional repo
+        mkdir -p ./yocto
+        if [[ ! -e "./yocto/meta-aos-rcar-gen4" ]]; then
+            git clone https://github.com/aoscloud/meta-aos-rcar-gen4 \
+                ./yocto/meta-aos-rcar-gen4
+        fi
+        cd ./yocto/meta-aos-rcar-gen4
+        git reset --hard ${AOS_VERSION}; git clean -df
+        PATCHSET=(
+            "0001-domd-Update-Linux-Kernel-to-latest-v5.10.41-rcar-5.1.patch"
+            "0002-domd-Add-Snort-and-required-S4-R-Switch-specific-lib.patch"
+            "0003-domd-Add-traffic-control-tool.patch"
+            "0004-README.md-Update-release-information-and-links.patch"
+            "0005-net-renesas-rswitch-fix-disabling-offload-with-defau.patch"
+        )
+        for patch in ${PATCHSET[@]}; do
+            URL=https://raw.githubusercontent.com/renesas-rcar/whitebox-sdk/${WHITEBOX_VERSION}/application_cpu/patchset_aos/${patch}
+            curl -s $URL | git apply
+        done
+    }
+    apply_patch_meta-aos-rcar-gen4
+
+    # Build image
+    cd ${SCRIPT_DIR}/work/s4_build
+    moulin ./aos-rcar-gen4-demo.yaml --BUILD_DOMD_SDK=no
+    ninja
+    ninja image-full
+    gzip -kf full.img
+    mv {,demo.}full.img.gz
+}
+
+#================================================
 # Extract ipl from proprietary package
-prepare_ipl () {
+#================================================
+prepare_ipl_spider () {
     OUTPUT_DIR=${SCRIPT_DIR}/work/s4_build/s4_ipl
     TEMP_DIR=${SCRIPT_DIR}/work/s4_build/s4_ipl_temp
     rm -rf ${OUTPUT_DIR} ${TEMP_DIR}
@@ -118,7 +166,14 @@ prepare_ipl () {
     cp -f ${SCRIPT_DIR}/work/s4_build/yocto/build-domd/tmp/deploy/images/spider/u-boot-elf-spider.srec \
         ${OUTPUT_DIR}/u-boot-elf.srec
 }
-prepare_ipl
+
+#================================================
+# main func
+#================================================
+proc_args $@
+check_proprietary_package_$TARGET_BOARD
+build_sw
+prepare_ipl_$TARGET_BOARD
 
 echo "Build finished !"
 
