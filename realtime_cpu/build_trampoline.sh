ef9df8160dd10da4beb864aae6318b6b5fe40a94
Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
2023-09-29 13:15:45 +0900

    cr52: trampoline: Add sample project
    -iccom and Benchmark integrated
    
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>

--- realtime_cpu/build_trampoline.sh
+++ realtime_cpu/build_trampoline.sh
@@ -63,6 +63,23 @@ if [[ ! -e ${SOURCE_DIR} || "$CLEAN_BUILD_FLAG" == "true" ]]; then
     if [ "$1" == "s4sk" ]; then
         git am ${SCRIPT_DIR}/patchset_trampoline_s4sk/*.patch
     fi
+
+    # Dhrystone
+    cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/sample
+    rm -rf ./dhrystone
+    wget -c https://fossies.org/linux/privat/old/dhrystone-2.1.tar.gz
+    mkdir -p ./dhrystone && tar xf dhrystone-2.1.tar.gz -C ./dhrystone
+    cp ./dhrystone/dhry_1.c{,.org}
+    cd ./dhrystone
+    patch -p0 < ${SCRIPT_DIR}/patchset_trampoline/sample_dhry_1.c.diff
+    # Coremark
+    cd ..
+    rm -rf ./coremark
+    git clone https://github.com/eembc/coremark ${SOURCE_DIR}/examples/cortex-a/armv8/spider/sample/coremark
+    cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/sample/coremark
+    git reset --hard d5fad6bd094899101a4e5fd53af7298160ced6ab ; git clean -df
+    git apply ${SCRIPT_DIR}/patchset_trampoline/sample_coremark.diff
+
 fi
 
 # Setup python build env
@@ -75,37 +92,13 @@ cd ${SOURCE_DIR}/goil/makefile-unix
 export PATH=${SOURCE_DIR}/goil/makefile-unix:${PATH}
 
 # build project
-cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/iccom
+cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/sample
 bash ./build.sh
 
 # deploy
 rm -rf ${SCRIPT_DIR}/deploy
 mkdir -p ${SCRIPT_DIR}/deploy
-arm-none-eabi-objcopy --adjust-vma 0xe2100000 -O srec --srec-forceS3 iccom_exe.elf $SCRIPT_DIR/deploy/cr52.srec
-
-# build benchmark
-## Setup source code
-### Dhrystone
-cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/benchmark
-rm -rf ./dhrystone
-wget -c https://fossies.org/linux/privat/old/dhrystone-2.1.tar.gz
-mkdir -p ./dhrystone && tar xf dhrystone-2.1.tar.gz -C ./dhrystone
-cp ./dhrystone/dhry_1.c{,.org}
-cd ./dhrystone
-patch -p0 < ${SCRIPT_DIR}/patchset_trampoline/dhry_1.c.diff
-### Coremark
-if [ ! -e ${SOURCE_DIR}/examples/cortex-a/armv8/spider/benchmark/coremark ]; then
-    git clone https://github.com/eembc/coremark ${SOURCE_DIR}/examples/cortex-a/armv8/spider/benchmark/coremark
-fi
-cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/benchmark/coremark
-git reset --hard d5fad6bd094899101a4e5fd53af7298160ced6ab ; git clean -df
-git apply ${SCRIPT_DIR}/patchset_trampoline/coremark.diff
-## Build
-cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/benchmark
-chmod +x ./build.sh
-./build.sh
-mkdir -p ${SCRIPT_DIR}/deploy
-arm-none-eabi-objcopy --adjust-vma 0xe2100000 -O srec --srec-forceS3 benchmark_exe.elf $SCRIPT_DIR/deploy/cr52_bench.srec
+arm-none-eabi-objcopy --adjust-vma 0xe2100000 -O srec --srec-forceS3 sample_exe.elf $SCRIPT_DIR/deploy/cr52.srec
 
 # build eathernet
 cd ${SOURCE_DIR}/examples/cortex-a/armv8/spider/ethernet
