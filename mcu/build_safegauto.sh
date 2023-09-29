d1666e7b4c96062df7fc47fb406739093ef550c9
Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
2023-09-29 13:43:21 +0900

    g4mh: safeg-auto: Add sample project
    -iccom and Benchmark integrated
    
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>

--- mcu/build_safegauto.sh
+++ mcu/build_safegauto.sh
@@ -53,8 +53,24 @@ if [[ ! -e ${SOURCE_DIR} || "$CLEAN_BUILD_FLAG" == "true" ]]; then
     rm a-rtegen-1.4.0.src.tar.gz
     mv vm_atk2/vm1_atk2/common/tool/a-rtegen vm_atk2/vm1_atk2/common/tool/a-rtegen-1.4.0
     git --git-dir= apply -p1 ${SCRIPT_DIR}/toppers/0001-rte-Add-compile-environment.patch
-fi
 
+    # benchmak sample
+    git --git-dir= apply -p1 ${SCRIPT_DIR}/patchset_safeg_sample/0001-Marge-benchmark.patch
+    cd ${SOURCE_DIR}/vm_atk2/vm1_atk2/
+    rm -rf ./dhrystone
+    wget -c https://fossies.org/linux/privat/old/dhrystone-2.1.tar.gz
+    mkdir -p ./dhrystone && tar xf dhrystone-2.1.tar.gz -C ./dhrystone
+    cp ./dhrystone/dhry_1.c{,.org}
+    cd ./dhrystone && patch -p0 < ${SCRIPT_DIR}/patchset_safeg_sample/dhry_1.c.diff
+
+    if [ ! -e ${SOURCE_DIR}/vm_atk2/vm1_atk2/coremark ]; then
+        git clone https://github.com/eembc/coremark ${SOURCE_DIR}/vm_atk2/vm1_atk2/coremark
+    fi
+    cd ${SOURCE_DIR}/vm_atk2/vm1_atk2/coremark
+    git reset --hard d5fad6bd094899101a4e5fd53af7298160ced6ab ; git clean -df
+    git apply ${SCRIPT_DIR}/patchset_safeg_sample/coremark.diff
+
+fi
 cd ${SOURCE_DIR}
 # set path
 opt1='./vm_atk2/vm1_atk2/common/'
@@ -79,7 +95,7 @@ cd $opt1
 sh configure.sh
 cd $opt3
 cd $opt2
-make 
+make
 cd $opt3
 make depend
 make
@@ -94,47 +110,3 @@ rm hypervisor.s3 atk2-sc1_vm1.s3 atk2-sc1_vm2.s3
 
 mkdir -p ${SCRIPT_DIR}/deploy
 cp -f ./G4MH.srec ${SCRIPT_DIR}/deploy/g4mh.srec
-
-
-# benchmakr sample
-SOURCE_DIR=${SCRIPT_DIR}/safeg-auto-bench
-if [[ ! -e ${SOURCE_DIR} || "$CLEAN_BUILD_FLAG" == "true" ]]; then
-    if [ ! -e ${SOURCE_DIR} ]; then
-        git clone https://github.com/toppers/safeg-auto.git ${SOURCE_DIR}
-    fi
-
-    cd ${SOURCE_DIR}
-    git reset --hard tags/v1.0.0 ; git clean -df
-
-    # prepare Benchmarks code
-    git am ${SCRIPT_DIR}/benchmark/*.patch
-
-    cd ${SOURCE_DIR}/vm_atk2/vm2_atk2/
-    rm -rf ./dhrystone
-    wget -c https://fossies.org/linux/privat/old/dhrystone-2.1.tar.gz
-    mkdir -p ./dhrystone && tar xf dhrystone-2.1.tar.gz -C ./dhrystone
-    cp ./dhrystone/dhry_1.c{,.org}
-    cd ./dhrystone && patch -p0 < ${SCRIPT_DIR}/benchmark/dhry_1.c.diff
-
-    if [ ! -e ${SOURCE_DIR}/vm_atk2/vm2_atk2/coremark ]; then
-        git clone https://github.com/eembc/coremark ${SOURCE_DIR}/vm_atk2/vm2_atk2/coremark
-    fi
-    cd ${SOURCE_DIR}/vm_atk2/vm2_atk2/coremark
-    git reset --hard d5fad6bd094899101a4e5fd53af7298160ced6ab ; git clean -df
-    git apply ${SCRIPT_DIR}/benchmark/coremark.diff
-fi
-
-# all make
-cd ${SOURCE_DIR}
-make
-
-rm -f G4MH.srec
-objcopy -O srec --srec-forceS3 ./hypervisor/obj/obj_1pe_atk2/hypervisor.elf hypervisor.s3
-objcopy -O srec --srec-forceS3 ./vm_atk2/vm1_atk2/atk2-sc1_vm1.elf atk2-sc1_vm1.s3
-objcopy -O srec --srec-forceS3 ./vm_atk2/vm2_atk2/atk2-sc1_vm2.elf atk2-sc1_vm2.s3
-
-rlink ../G4MH_Head.srec hypervisor.s3 atk2-sc1_vm1.s3 atk2-sc1_vm2.s3 -fo=Stype -ou=G4MH_bench.srec
-rm hypervisor.s3 atk2-sc1_vm1.s3 atk2-sc1_vm2.s3
-
-
-
