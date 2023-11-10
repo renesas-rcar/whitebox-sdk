36be343603f79b081a5538f79dcd7ea2e0a5a235
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2023-11-10 15:05:53 +0900

    Add build script
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

---
+++ build_s4.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -eu
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+cd $SCRIPT_DIR
+
+CLEAN_BUILD_FLAG=false
+Usage() {
+    echo "Usage:"
+    echo "    $0 board [option]"
+    echo "board:"
+    echo "    - spider: for S4 Spider"
+    echo "    - s4sk: for S4 Starter Kit"
+    echo "option:"
+    echo "    -c: Clean build flag(Defualt is disable)"
+    echo "    -h: Show this usage"
+}
+
+if [[ $# < 1 ]] ; then
+    echo -e "\e[31mERROR: Please select a board to build \e[m"
+    Usage; exit
+fi
+# $1 í board name; spider or s4sk
+if [ "$1" != "spider" ] && [ "$1" != "s4sk" ]; then
+    echo -e "\e[31mERROR: Please "input" correct board name: spider or s4sk\e[m"
+    Usage; exit
+fi
+BOARD=$1
+
+# Proc arguments
+OPTIND=2
+while getopts "ch" OPT
+do
+    case $OPT in
+        c) CLEAN_BUILD_FLAG=true;;
+        h) Usage; exit;;
+        *) echo -e "\e[31mERROR: Unsupported option\e[m"; Usage; exit;;
+    esac
+done
+
+# Prepare working directory
+cd ${SCRIPT_DIR}
+if [[ "$CLEAN_BUILD_FLAG" == true || ! -e ./work ]] ;then
+    rm -rf ./work
+    mkdir -p ./work
+fi
+cd ./work
+
+# Preprae yaml file
+cp -f ../aos-rcar-demo2023.yaml ./aos-rcar-demo2023.yaml
+# To avoid increasing network usage, using local repository
+if [[ -d $SCRIPT_DIR/common_data/repo ]]; then
+    bash ../setup_local_repository.sh
+    # replace network path with local filepath
+    # Replace zephyr path firstly: relative path is not same as yocto
+    sed -i -e 's|https://github.com/aoscloud/aos_core_zephyr.git|../../../common_data/repo/aos_core_zephyr|' \
+        ./aos-rcar-demo2023.yaml
+    sed -i -e 's|v0.2.1|s4sk-dev|' ./aos-rcar-demo2023.yaml
+    # Replace Yocto path
+    sed -i -e 's|url: ".*://.*/|url: "../common_data/repo/|' -e 's/\.git//' ./aos-rcar-demo2023.yaml
+fi
+
+moulin ./aos-rcar-demo2023.yaml \
+    --GEN3_DEVICE disable \
+    --GEN3_MACHINE h3ulcb-4x2g \
+    --DOM0 linux \
+    --GEN4_MACHINE $BOARD
+
+ninja
+ninja gen4_full.img
+gzip -f gen4_full.img
+
+exit
+
+ninja gen3_full.img
+gzip -f gen3_full.img
+
---
+++ setup_local_repository.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+SCRIPT_DIR=$(cd `dirname $0` && pwd)
+repolist=$(grep -h url ./*.yaml | grep -v "^#" | awk '{print $2}' | sed 's/"//g' )
+REPO_DIR=${SCRIPT_DIR}/common_data/repo
+
+mkdir -p $REPO_DIR
+for repo in ${repolist[@]}; do
+    DIR_NAME=$(echo $repo | rev | cut -d'/' -f1 | rev | sed 's/.git//')
+    # echo $repo: dir_name is $DIR_NAME
+    if [ ! -d ${REPO_DIR}/${DIR_NAME} ]; then
+        git clone $repo ${REPO_DIR}/${DIR_NAME}
+    fi
+    cd ${REPO_DIR}/${DIR_NAME} && git pull
+done
+
