521561c250eaea46e6c1eada8e0cedfaf9fd873a
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2023-11-29 12:27:58 +0900

    gen3: Add api-server
    This is used for reconnecting camera streaming on Grafana
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

---
+++ meta-whitebox-demo/recipes-demo-gen3/api-server/api-server.bb
@@ -0,0 +1,32 @@
+DESCRIPTION = "API server for executing command using HTTP protocol"
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+inherit systemd
+SYSTEMD_AUTO_ENABLE = "enable"
+SYSTEMD_SERVICE_FILENAME = "${PN}.service"
+SYSTEMD_SERVICE_${PN} = "${SYSTEMD_SERVICE_FILENAME}"
+
+SRC_URI_append = " \
+    file://${BPN}.py \
+    file://${SYSTEMD_SERVICE_FILENAME} \
+"
+
+# do_configure() nothing
+do_configure[noexec] = "1"
+# do_compile() nothing
+do_compile[noexec] = "1"
+
+do_install() {
+    # service file
+   install -d ${D}/${systemd_unitdir}/system
+   install -m 0644 ${WORKDIR}/${SYSTEMD_SERVICE_FILENAME} ${D}/${systemd_unitdir}/system
+
+   # install execution file
+   install -d ${D}/${USRBINPATH}/${PN}
+   install -m 0644 ${WORKDIR}/${PN}.py ${D}/${USRBINPATH}/${PN}
+}
+
---
+++ meta-whitebox-demo/recipes-demo-gen3/api-server/files/api-server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from http import HTTPStatus
+from urllib.parse import urlparse, parse_qs
+
+def eprint(*args, **kwargs):
+    print("Log: ", end='', file=sys.stderr)
+    print(*args, file=sys.stderr, **kwargs)
+
+class HttpHandler(BaseHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With, Content-type")
+
+    def do_GET(self):
+        eprint(self.path[1:])
+        host = urlparse(self.path[1:]).netloc
+        command = urlparse(self.path).path[1:]
+        query = urlparse(self.path).query
+        query_dict = parse_qs(query)
+        args = ""
+        for value in query_dict.values():
+            args += value[0] + " "
+
+        # Print command
+        _command = args.rstrip()
+        print(_command)
+        sys.stdout.flush()
+
+        # Response to client
+        ## make header
+        self.send_response(HTTPStatus.OK)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        ## Make response
+        self.wfile.write(b"Hello")
+    def log_message(self, format, *args):
+        return
+
+if __name__ == '__main__':
+    server_address = ('0.0.0.0', 8000)
+    eprint(server_address)
+    httpd = HTTPServer(server_address, HttpHandler)
+    httpd.serve_forever()
+
