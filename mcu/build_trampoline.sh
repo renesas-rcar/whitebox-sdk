36bb7cf22a66ad9fb4f50b8d4b0feb773b83216b
Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
2023-12-12 18:43:42 +0900

    g4mh: trampoline update base version
    Signed-off-by: Tsutomu Muroya <tsutomu.muroya.jy@bp.renesas.com>
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- mcu/build_trampoline.sh
+++ mcu/build_trampoline.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(cd `dirname $0` && pwd)
 GITHUB_URL=https://github.com/TrampolineRTOS/trampoline
-COMMIT=5deaff4941cb086e25859743973af019f2bad27c
+COMMIT=d930f9447a237feb062ef3040b980cb9d2b748e7
 SOURCE_DIR=${SCRIPT_DIR}/trampoline
 
 export PATH=~/.local/bin:$PATH
@@ -57,7 +57,7 @@ if [[ ! -e ${SOURCE_DIR} || "$CLEAN_BUILD_FLAG" == "true" ]]; then
     git am ${SCRIPT_DIR}/patchset_trampoline/*.patch
 
     # Dhrystone
-    cd ${SOURCE_DIR}/examples/renesas/sample
+    cd ${SOURCE_DIR}/examples/rh850/sample
     rm -rf ./dhrystone
     wget -c https://fossies.org/linux/privat/old/dhrystone-2.1.tar.gz
     mkdir -p ./dhrystone && tar xf dhrystone-2.1.tar.gz -C ./dhrystone
@@ -66,19 +66,19 @@ if [[ ! -e ${SOURCE_DIR} || "$CLEAN_BUILD_FLAG" == "true" ]]; then
     # Coremark
     cd ..
     rm -rf ./coremark
-    git clone https://github.com/eembc/coremark ${SOURCE_DIR}/examples/renesas/sample/coremark
-    cd ${SOURCE_DIR}/examples/renesas/sample/coremark
+    git clone https://github.com/eembc/coremark ${SOURCE_DIR}/examples/rh850/sample/coremark
+    cd ${SOURCE_DIR}/examples/rh850/sample/coremark
     git reset --hard d5fad6bd094899101a4e5fd53af7298160ced6ab ; git clean -df
     git apply ${SCRIPT_DIR}/patchset_trampoline/sample_coremark.diff
 fi
 
 # Setup uart baudrate
 if [ "$1" == "s4sk" ]; then
-    sed -i 's/-DHSCIF_1843200BPS/-DHSCIF_921600BPS/g' ${SOURCE_DIR}/examples/renesas/sample/sample.oil
-    sed -i 's/-DHSCIF_1843200BPS/-DHSCIF_921600BPS/g' ${SOURCE_DIR}/examples/renesas/sample/sample_not_can.oil
+    sed -i 's/-DHSCIF_1843200BPS/-DHSCIF_921600BPS/g' ${SOURCE_DIR}/examples/rh850/sample/sample.oil
+    sed -i 's/-DHSCIF_1843200BPS/-DHSCIF_921600BPS/g' ${SOURCE_DIR}/examples/rh850/sample/sample_not_can.oil
 elif [ "$1" == "spider" ];  then
-    sed -i 's/-DHSCIF_921600BPS/-DHSCIF_1843200BPS/g' ${SOURCE_DIR}/examples/renesas/sample/sample.oil
-    sed -i 's/-DHSCIF_921600BPS/-DHSCIF_1843200BPS/g' ${SOURCE_DIR}/examples/renesas/sample/sample_not_can.oil
+    sed -i 's/-DHSCIF_921600BPS/-DHSCIF_1843200BPS/g' ${SOURCE_DIR}/examples/rh850/sample/sample.oil
+    sed -i 's/-DHSCIF_921600BPS/-DHSCIF_1843200BPS/g' ${SOURCE_DIR}/examples/rh850/sample/sample_not_can.oil
 fi
 
 if [[ "$(which rlink | grep no)" != "" ]]; then
@@ -102,24 +102,24 @@ rm -rf ${SCRIPT_DIR}/deploy
 mkdir -p ${SCRIPT_DIR}/deploy
 
 # build sample
-cd ${SOURCE_DIR}/examples/renesas/sample
+cd ${SOURCE_DIR}/examples/rh850/sample
 chmod +x ./build.sh
 ./build.sh
 cd ${SOURCE_DIR}
 rm -f G4MH_sample.srec
-objcopy -O srec --srec-forceS3 ${SOURCE_DIR}/examples/renesas/sample/_build/sample_exe.abs sample_exe.s3
+objcopy -O srec --srec-forceS3 ${SOURCE_DIR}/examples/rh850/sample/_build/sample_exe.abs sample_exe.s3
 rlink ../G4MH_Head.srec sample_exe.s3 -fo=Stype -ou=G4MH_sample.srec
 rm -f sample_exe.s3
 cp -f G4MH_sample.srec ${SCRIPT_DIR}/deploy/g4mh.srec
 
 # build sample(can disable)
 if [ "$1" == "s4sk" ]; then
-    cd ${SOURCE_DIR}/examples/renesas/sample
+    cd ${SOURCE_DIR}/examples/rh850/sample
     chmod +x ./build_not_can.sh
     ./build_not_can.sh
     cd ${SOURCE_DIR}
     rm -f G4MH_sample.srec
-    objcopy -O srec --srec-forceS3 ${SOURCE_DIR}/examples/renesas/sample/_build/sample_exe.abs sample_exe.s3
+    objcopy -O srec --srec-forceS3 ${SOURCE_DIR}/examples/rh850/sample/_build/sample_exe.abs sample_exe.s3
     rlink ../G4MH_Head.srec sample_exe.s3 -fo=Stype -ou=G4MH_sample.srec
     rm -f sample_exe.s3
     cp -f G4MH_sample.srec ${SCRIPT_DIR}/deploy/g4mh_can_disable.srec
