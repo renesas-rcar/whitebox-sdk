e4e1ab13fc997ceb5080edf01aa705079a9563d1
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2024-12-10 16:20:50 +0900

    tool: can_demo: Fix socketcan setip
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- tool/for_host_pc_setup/can_demo/socketcan_setup.sh
+++ tool/for_host_pc_setup/can_demo/socketcan_setup.sh
@@ -8,5 +8,5 @@ fi
 
 set -x
 for CAN_DEV in "${CAN_DEV_LIST[@]}"; do
-    sudo ip link set $CAN_DEV down; sudo ip link set $CAN_DEV up type can bitrate 500000 restart-ms 100 dbitrate 2000000 fd on
+    sudo ip link set $CAN_DEV down; sudo ip link set $CAN_DEV up type can bitrate 500000 restart-ms 100 dbitrate 2000000 fd on sample-point 0.80
 done
