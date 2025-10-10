7388f78a824e3c6901cc14e92025b946c0ad9917
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-10-10 20:51:15 +0900

    tool: setup_ca55: Add missing package for Ubuntu 24.04
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- tool/setup_ca55.sh
+++ tool/setup_ca55.sh
@@ -6,3 +6,9 @@ export PATH=~/.local/bin:$PATH
 pipx install git+https://github.com/xen-troops/moulin
 sudo apt install ninja-build
 
+# For Ubuntu 24.04
+UBUNTU_VERSION=$( cat /etc/os-release | grep VERSION_ID | cut -d'"' -f2)
+if [[ "${UBUNTU_VERSION}" == "24.04" ]]; then
+    sudo apt install python3-pyasyncore -y
+fi
+
