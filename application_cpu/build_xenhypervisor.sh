a44cd0b8361639fd3f1f861b986e74ee77f27080
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-10-31 12:02:01 +0900

    ca55: Fix yocto build directory path
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- application_cpu/build_xenhypervisor.sh
+++ application_cpu/build_xenhypervisor.sh
@@ -82,17 +82,17 @@ ninja || ninja
 ninja image-full
 
 mkdir -p ${SCRIPT_DIR}/deploy
-cp yocto/build-domd/tmp/deploy/images/${1}/bl31-${1}.srec ${SCRIPT_DIR}/deploy/
-cp yocto/build-domd/tmp/deploy/images/${1}/tee-${1}.srec ${SCRIPT_DIR}/deploy/
-cp yocto/build-domd/tmp/deploy/images/${1}/u-boot-elf-${1}.srec ${SCRIPT_DIR}/deploy/
+cp yocto-$1/build-domd/tmp/deploy/images/${1}/bl31-${1}.srec ${SCRIPT_DIR}/deploy/
+cp yocto-$1/build-domd/tmp/deploy/images/${1}/tee-${1}.srec ${SCRIPT_DIR}/deploy/
+cp yocto-$1/build-domd/tmp/deploy/images/${1}/u-boot-elf-${1}.srec ${SCRIPT_DIR}/deploy/
 
 mv -f full.img $1.${BOOT_DEV}.full.img
 gzip -f $1.${BOOT_DEV}.full.img
 cp -f $1.${BOOT_DEV}.full.img.gz -t ${SCRIPT_DIR}/deploy
 
-if [[ -e "${SCRIPT_DIR}/work/yocto/build-domd/tmp/deploy/sdk" ]]; then
-    find ${SCRIPT_DIR}/work/yocto/build-domd/tmp/deploy/sdk/ -name *.sh | xargs cp -f -t ${SCRIPT_DIR}/work
-    find ${SCRIPT_DIR}/work/yocto/build-domd/tmp/deploy/sdk/ -name *.sh | xargs cp -f -t ${SCRIPT_DIR}/deploy
+if [[ -e "${SCRIPT_DIR}/work/yocto-$1/build-domd/tmp/deploy/sdk" ]]; then
+    find ${SCRIPT_DIR}/work/yocto-$1/build-domd/tmp/deploy/sdk/ -name *.sh | xargs cp -f -t ${SCRIPT_DIR}/work
+    find ${SCRIPT_DIR}/work/yocto-$1/build-domd/tmp/deploy/sdk/ -name *.sh | xargs cp -f -t ${SCRIPT_DIR}/deploy
 fi
 
 echo "Build finished !"
