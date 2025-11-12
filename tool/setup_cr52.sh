1b6c06eb3033d3fa79a3a9ec5cfcce7a2d471ccd
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2025-11-12 20:22:56 +0900

    tool: cr52: Remove multilib package
    We have used cross-compiler so that multilib is not needed.
    This patch removes it.
    
    Signed-off-by: Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>

--- tool/setup_cr52.sh
+++ tool/setup_cr52.sh
@@ -10,5 +10,5 @@ sudo apt install --no-install-recommends --allow-downgrades \
   git cmake=3.* cmake-data=3.* ninja-build gperf \
   ccache dfu-util device-tree-compiler wget \
   python3-dev python3-pip python3-setuptools python3-tk python3-wheel xz-utils file \
-  make gcc gcc-multilib g++-multilib libsdl2-dev libmagic1 -y
+  make gcc libsdl2-dev libmagic1 -y
 
